a79ad2f803b9810c0111ee542c79873a3f07540c
noobDevUmar <m.umarqureshi091@gmail.com>
2023-12-09 09:55:41 -0800

    payload added , middlewares started

--- redux/index.js
+++ redux/index.js
@@ -1,26 +1,34 @@
-import {createStore} from 'redux'
-
+import {createStore,applyMiddleware} from 'redux'
+import logger from 'redux-logger'
 // Store
 
-const store = createStore(reducer)
+const store = createStore(reducer,applyMiddleware(logger.default))
 
 
 // Reducer function takes state and retursn new state,  here we have give an initial value of amount for practice..
 
 function reducer(state={amount:1},action){
     if(action.type=== 'increment'){
-        return {amount:state.amount+=1}
+        return {amount:state.amount+1}
+    }
+    if(action.type=== 'decrement'){
+        return {amount:state.amount-1}
+    }
+    if(action.type=== 'incrementByAmount'){
+        return {amount:state.amount+ action.payload}
     }
     return state
     }
 
 
 // global state
-console.log(store.getState());
+// console.log(store.getState());
 
 // Dispatch is a method through which action is passed to the reducer for updateing state
 // type is the random name for actions ..
-store.dispatch({type:'increment'})
+// store.dispatch({type:'increment'})
 
-// Here the value will be updated
-console.log(store.getState());
+
+setInterval(()=>{
+    store.dispatch({type:'incrementByAmount',payload:4})
+},2000)
